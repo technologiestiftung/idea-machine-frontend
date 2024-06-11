e9b781f87c7b35d1ea56dc92039aae93dac7cf42
aeschi <anna.eschenbacher@icloud.com>
2024-06-11 15:44:19 +0200

    feat: add flip animation

--- src/components/postcard.tsx
+++ src/components/postcard.tsx
@@ -1,12 +1,22 @@
 import { Idea } from "../types.ts";
 import { Frontside } from "./frontside";
 import { Backside } from "./backside";
+import { useState } from "react";
 
 export function Postcard({ data }: { data: Idea }) {
+	const [isBackVisible, setIsBackVisible] = useState(false);
+
 	return (
-		<div className="w-[423px] h-[600px] border rounded flex flex-col gap-2 shadow-lg">
-			<Frontside url={data.illustration_url} />
-			<Backside data={data} />
+		<div
+			onClick={() => setIsBackVisible(!isBackVisible)}
+			className={`w-[423px] h-[300px] border rounded flex flex-col gap-2 shadow-lg relative preserve-3d ${isBackVisible ? "my-rotate-y-180 duration-1000" : "my-rotate-y-0 duration-1000"}`}
+		>
+			<div className="absolute backface-hidden ">
+				<Backside data={data} />
+			</div>
+			<div className="absolute my-rotate-y-180 backface-hidden overflow-hidden">
+				<Frontside url={data.illustration_url} />
+			</div>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const PostcardFlip = plugin(function ({ addUtilities }) {
+	addUtilities({
+		".my-rotate-y-180": {
+			transform: "rotateY(180deg)",
+		},
+		".my-rotate-y-0": {
+			transform: "rotateY(0deg)",
+		},
+		".preserve-3d": {
+			transformStyle: "preserve-3d",
+		},
+		".perspective": {
+			perspective: "1000px",
+		},
+		".backface-hidden": {
+			backfaceVisibility: "hidden",
+		},
+	});
+});
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
@@ -11,5 +33,5 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [PostcardFlip],
 };
