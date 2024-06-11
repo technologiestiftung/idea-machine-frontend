1e1cd90ee9292f465b446f23160abfa97004ec4f
aeschi <anna.eschenbacher@icloud.com>
2024-06-11 16:07:09 +0200

    feat: add basic random positioning

--- src/app.tsx
+++ src/app.tsx
@@ -6,14 +6,24 @@ import { dbStore } from "./store/store";
 
 const nodeTypes = { postcard: Postcard };
 
+const getRandomCircularCoordinates = () => {
+	// https://stackoverflow.com/questions/5837572/generate-a-random-point-within-a-circle-uniformly
+	const t = 2 * Math.PI * Math.random();
+	const r = Math.sqrt(Math.random());
+	const x = 0.5 + r * Math.cos(t) * 0.75;
+	const y = 0.5 + r * Math.sin(t) * 0.75;
+
+	return { x: x * 1500, y: y * 1000 };
+};
+
 export default function App() {
 	const ideas = useSyncExternalStore(dbStore.subscribe, dbStore.getSnapshot);
 
 	const nodes = useMemo(
 		() =>
-			ideas.map((row, index) => ({
+			ideas.map((row) => ({
 				id: row.id,
-				position: { x: (index % 6) * 500, y: Math.floor(index / 6) * 700 },
+				position: getRandomCircularCoordinates(),
 				data: row,
 				type: "postcard",
 			})),
--- src/components/postcard.tsx
+++ src/components/postcard.tsx
@@ -9,9 +9,11 @@ export function Postcard({ data }: { data: Idea }) {
 	return (
 		<div
 			onClick={() => setIsBackVisible(!isBackVisible)}
-			className={`w-[423px] h-[300px] border rounded flex flex-col gap-2 shadow-lg relative preserve-3d ${isBackVisible ? "my-rotate-y-180 duration-1000" : "my-rotate-y-0 duration-1000"}`}
+			className={`w-[423px] h-[300px] border rounded flex flex-col gap-2 shadow-lg relative preserve-3d 
+			${isBackVisible ? "my-rotate-y-180 duration-1000" : "my-rotate-y-0 duration-1000"}
+			`}
 		>
-			<div className="absolute backface-hidden ">
+			<div className="absolute backface-hidden">
 				<Backside data={data} />
 			</div>
 			<div className="absolute my-rotate-y-180 backface-hidden overflow-hidden">
