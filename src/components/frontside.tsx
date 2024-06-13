f2219c4ed463e19e91e996e429536ec8785cbd74
aeschi <anna.eschenbacher@icloud.com>
2024-06-13 13:18:00 +0200

    feat: add special font

--- src/components/frontside.tsx
+++ src/components/frontside.tsx
@@ -3,12 +3,12 @@ import { Illustration } from "./illustration";
 export function Frontside({ url }: { url: string | null }) {
 	return (
 		<div className="w-[423px] h-[300px] bg-white flex justify-center p-4 relative rounded-md">
-			<div className="absolute -rotate-6 top-[21px] left-[11.5px] text-sm font-bold text-primaryBlue w-[90px] text-center">
+			<h4 className="absolute -rotate-6 top-[21px] left-[11.5px] text-sm font-bold text-primaryBlue w-[90px] text-center">
 				Grüße aus der Zukunft!
-			</div>
-			<div className="absolute -rotate-6 top-[20px] left-[12px] text-sm font-bold text-primaryPink w-[90px] text-center">
+			</h4>
+			<h4 className="absolute -rotate-6 top-[20px] left-[12px] text-sm font-bold text-primaryPink w-[90px] text-center">
 				Grüße aus der Zukunft!
-			</div>
+			</h4>
 			<Illustration file={url} />
 		</div>
 	);
