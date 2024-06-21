93412959971108590e113c9fde1038a390438fdb
aeschi <56318362+aeschi@users.noreply.github.com>
2024-06-21 17:35:21 +0200

    Feat/remove controls when mobile (#17)
    * feat: remove controls when width below 500px
    
    * feat: expanding text width for mobile view

--- src/components/frontside.tsx
+++ src/components/frontside.tsx
@@ -5,10 +5,10 @@ import { Idea } from "../types.ts";
 export function Frontside({ data }: { data: Idea }) {
 	return (
 		<div className="w-[423px] h-[300px] bg-white flex justify-center p-4 relative rounded-md">
-			<h4 className="absolute -rotate-6 top-[16px] left-[11.5px] text-lg leading-5 font-bold text-primaryBlue w-[90px] text-center">
+			<h4 className="absolute -rotate-6 top-[16px] left-[11.5px] text-lg leading-5 font-bold text-primaryBlue w-[95px] text-center">
 				Grüße aus der Zukunft!
 			</h4>
-			<h4 className="absolute -rotate-6 top-[15px] left-[12px] text-lg leading-5 font-bold text-primaryPink w-[90px] text-center">
+			<h4 className="absolute -rotate-6 top-[15px] left-[12px] text-lg leading-5 font-bold text-primaryPink w-[95px] text-center">
 				Grüße aus der Zukunft!
 			</h4>
 			<Illustration file={data.illustration_url} />
